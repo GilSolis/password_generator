c486810bedf4be314a782546900115376f595fc8
Gil Solis <gilsolisuw@gmail.com>
2019-12-23 23:53:55 -0800

    adding finishing touches on styling + slider js

--- script.js
+++ script.js
@@ -27,14 +27,30 @@
 //     var correctPasswordLength = prompt("Pick a number between 8-128."); 
 // }
 
-do { let passwordLength = prompt("How long do you want your password to be?  Pick a number between 8-128 for password length.");
-}
-while (passwordLength < 8 || passwordLength > 128)
+// do { let passwordLength = prompt("How long do you want your password to be?  Pick a number between 8-128 for password length.");
+// }
+// while (passwordLength < 8 || passwordLength > 128)
 
-console.log(passwordLength);
+// console.log(passwordLength);
 
-let numbers = ['0','1','2','3','4','5','6','7','8','9'];
-let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-let upperCase = lowerCase.map(function(x){ return x.toUpperCase() })
-let specialCharacters = []
+// let numbers = ['0','1','2','3','4','5','6','7','8','9'];
+// let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// let upperCase = lowerCase.map(function(x){ return x.toUpperCase() })
+// let specialCharacters = []
  
+
+document.getElementById("length").innerHTML = "Length: 60";
+
+// let passwordSize = document.getElementById("slider")
+
+document.getElementById("slider").oninput = function(){
+    if (document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+    }
+    else {
+        document.getElementById("length").innerHTML = "Length: 1";
+
+    }
+
+
+}
