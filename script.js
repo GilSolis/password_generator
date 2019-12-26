d21bc3b27642934eeb22389f35fcdd9e03b97216
Gil Solis <gilsolisuw@gmail.com>
2019-12-26 12:04:33 -0800

    finalizing choices array

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Setting the variables
+// Setting the variables
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var numbersChosen = document.getElementById("numbers");
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -8,16 +8,42 @@ var upperCaseChosen = document.getElementById("upperCase");
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 var specialCharactersChosen = document.getElementById("specialChars");
 // var passwordSize = document.getElementById("slider").value;
-
+// var password = [];
 // console.log(upperCase);
 
 console.log(specialCharactersChosen.checked);
 
+// generate password
+document.getElementById("generate").addEventListener("click", function() {
 
-if(numbersChosen.checked == false && lowerCaseChosen.checked == false && upperCaseChosen.checked == false && specialCharactersChosen.checked == false ){document.getElementById("password").innerHTML = "Must choose at least one option"};
+    if(numbersChosen.checked == false && lowerCaseChosen.checked == false && upperCaseChosen.checked == false && specialCharactersChosen.checked == false ){document.getElementById("display").innerHTML = "Must choose at least one option"};
+
+// setting choice array
+    var choices = [];
 
 
+// adding choice to array
+    if(numbersChosen.checked === true){
+    choices = choices.concat(numbers);    
+    }
+    if(lowerCaseChosen.checked === true){
+    choices = choices.concat(lowerCase);
+    }
+    if(upperCaseChosen.checked === true){
+    choices = choices.concat(upperCase);    
+    }
+    if(specialCharactersChosen.checked === true){
+    choices = choices.concat(specialCharacters);    
+    }
+console.log(choices.length)
+// setting password array
 
+var password = [];
+
+
+// console.log(choices);
+
+});
 // create array of characters based on users choice
 
 // create loop based on # of character requirement put that index into a new password variable.
@@ -42,9 +68,8 @@ if(numbersChosen.checked == false && lowerCaseChosen.checked == false && upperCa
 // console.log(passwordLength);
 
 
- 
 
-document.getElementById("length").innerHTML = "Length: 60";
+document.getElementById("length").innerHTML = "Length: 60"
 
 // let passwordSize = document.getElementById("slider")
 
